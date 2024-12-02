686e04f0c97a4e99783f60f5a28d913bed5c3308
Emmanuel Vera <emm.vera@gmail.com>
2024-12-02 11:56:21 -0600

    Add day 01 part 2
    Signed-off-by: Emmanuel Vera <emm.vera@gmail.com>

--- 2024/rust/day-01/src/lib.rs
+++ 2024/rust/day-01/src/lib.rs
@@ -16,19 +16,44 @@ pub fn process_part1(input: &str) -> String {
     return result;
 }
 
+pub fn process_part2 (input: &str) -> String {
+    let mut l = Vec::new();
+    let mut r = Vec::new();
+    input.lines().for_each(|side_side_ids| {
+        let mut ints = Vec::new();
+        side_side_ids.split_whitespace().for_each(|item| ints.push(item.parse::<i32>().unwrap()));
+        l.push(ints[0]);
+        r.push(ints[1]);
+    });
+
+    let result : String = l.into_iter().map(|val| {
+        let count : i32 = r.clone().into_iter().filter(|num| *num == val).count() as i32;
+        count*val
+    }).sum::<i32>().to_string();
+
+    return result;
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn part1_test() {
-        let input = "3   4
+        const INPUT : &str = "3   4
 4   3
 2   5
 1   3
 3   9
 3   3";
-        let result = process_part1(&input);
+
+    #[test]
+    fn part1_test() {
+        let result = process_part1(&INPUT);
         assert_eq!(result, "11");
+    }
+
+    #[test]
+    fn part2_test() {
+        let result = process_part2(&INPUT);
+        assert_eq!(result, "31");
     }
 }
